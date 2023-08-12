d7eaa48c759dd5e739d11602d571fe47993ca017
lennyme <lennystudios@gmail.com>
2023-08-12 17:53:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-const widgetContainer = document.querySelector("#MeasuringCX-widget-container");
-const NOTION_SECRET = "secret_MAD79YjUTt8HpdYjAn8C7iVeLSqjsysbCZF9FKjQVA0";
-const NOTION_DATABASE_ID = "d3e72fa3c5c54591ae5533cfe06ed750";
+const NOTION_SECRET = 'secret_MAD79YjUTt8HpdYjAn8C7iVeLSqjsysbCZF9FKjQVA0';
+const NOTION_DATABASE_ID = 'd3e72fa3c5c54591ae5533cfe06ed750';
+const NOTION_API_ENDPOINT = 'https://api.notion.com/v1/pages';
 
-async function fetchPosts() {
-  try {
-    const response = await fetch('https://api.MeasuringCX.com/posts', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
+async function getUserIP() {
+    const response = await fetch('https://api.ipify.org?format=json');
     const data = await response.json();
-    await sendToNotion(data);
-  } catch (error) {
-    console.error('There was a problem:', error);
-  }
+    return data.ip;
 }
 
-async function sendToNotion(posts) {
-  for (let post of posts) {
+async function sendFeedbackToNotion(content) {
+    const userIP = await getUserIP();
+
     const payload = {
-      "parent": { "database_id": NOTION_DATABASE_ID },
-      "properties": {
-        "Title": { "title": [{ "text": { "content": post.title } }] },
-        "Content": { "rich_text": [{ "text": { "content": post.body } }] }
-      }
+        "parent": { "database_id": NOTION_DATABASE_ID },
+        "properties": {
+            "Feedback": {
+                "text": [
+                    { "text": { "content": content } }
+                ]
+            },
+            "Ip": {
+                "text": [
+                    { "text": { "content": userIP } }
+                ]
+            }
+        }
     };
 
-    try {
-      await fetch('https://api.notion.com/v1/pages', {
+    const response = await fetch(NOTION_API_ENDPOINT, {
         method: 'POST',
         headers: {
-          'Authorization': `Bearer ${NOTION_SECRET}`,
-          'Content-Type': 'application/json',
-          'Notion-Version': '2021-05-13'
+            'Authorization': 'Bearer ' + NOTION_SECRET,
+            'Content-Type': 'application/json',
+            'Notion-Version': '2021-05-13'
         },
         body: JSON.stringify(payload)
-      });
-    } catch (error) {
-      console.error(`Error sending post "${post.title}" to Notion:`, error);
-    }
-  }
+    });
+
+    return response.json();
 }
 
-document.addEventListener("DOMContentLoaded", fetchPosts);
+document.querySelector('#feedbackButton').addEventListener('click', async () => {
+    const feedback = document.querySelector('#feedbackInput').value;
+    if (feedback) {
+        const result = await sendFeedbackToNotion(feedback);
+        if (result.id) {
+            alert('Feedback enviado exitosamente!');
+        } else {
+            alert('Hubo un error al enviar el feedback.');
+        }
+    } else {
+        alert('Por favor, ingresa tu feedback.');
+    }
+});
